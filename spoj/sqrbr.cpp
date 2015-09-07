a42f72f324b21811b7219ea3de839f4b747cf730
Gustavo Bitencourt <lg.bitencourt@gmail.com>
2015-09-07 19:42:27 -0300

    problema do satanas

--- spoj/sqrbr.cpp
+++ spoj/sqrbr.cpp
@@ -4,28 +4,34 @@
 using namespace std;
 
 int ks[40];
+int mdp[40][40];
 int n;
 
-int dp(int s, int j) {
-    if (j == 0 || j == n) return 1;
+int dp(int i, int j) {
+    if (j < i) return 0;
+    if (i + 1 == j && ks[j] != 1) return 1;
+    if (mdp[i][j] != -1) return mdp[i][j];
+
     int ans = 0;
-    for (int i = s + 1; i < j + s; i++) {
-        if (ks[i] == 1) continue;
-        ans += dp(s, i - s + 1) * dp(i + 1, j - i - 1);
-    }
-    return ans;
+    for (int k = i + 1; k < j; k++)
+        ans += dp(i + 1, k - 1) * dp(k + 1, j);
+    return mdp[i][j] = ans;
 }
 
 int main() {
+    ios::sync_with_stdio(false);
     int t, k;
     cin >> t;
     while (t--) {
         cin >> n >> k;
+        for (int i = 0; i < 40; i++)
+            for (int j = 0; j < 40; j++)
+                mdp[i][j] = -1;
         for (int i = 0; i < k; i++) {
             int s;
             cin >> s;
-            ks[--s] = 1;
+            ks[s] = 1;
         }
-        cout << dp(0, 2*n);
+        cout << dp(1, 2*n) << '\n';
     }
 }
