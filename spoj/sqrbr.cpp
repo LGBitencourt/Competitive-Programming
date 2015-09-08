4d6d321640fb2ed97a5500220c2b6ad7b8ab99fc
luis gustavo bitencourt almeida <luis.almeida@metralha.cec.ime.usp.br>
2015-09-08 15:52:58 -0300

    problema do satanas resolvido

--- spoj/sqrbr.cpp
+++ spoj/sqrbr.cpp
@@ -1,37 +1,35 @@
 #include <iostream>
 #include <algorithm>
+#include <cstring>
 
 using namespace std;
 
-int ks[40];
-int mdp[40][40];
-int n;
-
-int dp(int i, int j) {
-    if (j < i) return 0;
-    if (i + 1 == j && ks[j] != 1) return 1;
-    if (mdp[i][j] != -1) return mdp[i][j];
-
-    int ans = 0;
-    for (int k = i + 1; k < j; k++)
-        ans += dp(i + 1, k - 1) * dp(k + 1, j);
-    return mdp[i][j] = ans;
+int mdp[100][100];
+int ks[100];
+int n,k;
+int dp(int i,int d) {
+    if(d < 0)return 0;
+    if(i == n + 1) return d == 0;
+    if(mdp[i][d] != -1) return mdp[i][d];
+    if(ks[i]) return mdp[i][d] = dp(i + 1, d + 1);
+    return mdp[i][d] = dp(i + 1, d + 1) + dp(i + 1, d - 1);
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    int t, k;
+    int t;
     cin >> t;
-    while (t--) {
+    while(t--) {
+        memset(ks, 0, sizeof(ks));
+        memset(mdp, -1, sizeof(mdp));
         cin >> n >> k;
-        for (int i = 0; i < 40; i++)
-            for (int j = 0; j < 40; j++)
-                mdp[i][j] = -1;
-        for (int i = 0; i < k; i++) {
-            int s;
-            cin >> s;
-            ks[s] = 1;
+        n<<=1;
+        for(int i=1; i<= k; i++) {
+            int temp;
+            cin >> temp;
+            ks[temp] = 1;            
         }
-        cout << dp(1, 2*n) << '\n';
+        cout << dp(1, 0) << endl;
     }
+    return 0;
 }
+
