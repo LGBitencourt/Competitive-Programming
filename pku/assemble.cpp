84664dd5b41811edf57ef9b136c0cda6753a0dec
Luis Gustavo Bitencourt Almeida <ahluis@terra.eclipse.ime.usp.br>
2015-09-21 16:13:15 -0300

    maximizar minimo

--- pku/assemble.cpp
+++ pku/assemble.cpp
@@ -3,8 +3,10 @@
 #include <map>
 #include <utility>
 #include <vector>
+#include <climits>
+#include <string>
 
-#define MAX 10000005
+#define MAX 1000000005
 #define pii pair<int, int>
 
 using namespace std;
@@ -24,21 +26,33 @@ int main() {
             cin >> type_s >> trash >> price >> quality;
             if (type.find(type_s) == type.end())
                 type.insert(make_pair(type_s, types++));
-            components[type[type_s]].push_back(make_pair(price, quality));
+            components[type[type_s]].push_back(make_pair(quality, price));
         }
 
         int lo = 0, hi = MAX, mid;
         while (lo != hi) {
             mid = (lo + hi + 1)/2;
-            /*int i, j;
-            for (i = 0; i < n; i++) {
-                for (j = 0; j < components[i].size() && components[i][j].second <= mid; j++)
-                if (j != components[i].size()) break;
+            bool cagou = false;
+            int o = 0;
+            for (int i = 0; i < types; i++) {
+                int c = INT_MAX;
+                for (int j = 0; j < components[i].size(); j++)
+                    if (components[i][j].first >= mid && components[i][j].second < c)
+                        c = components[i][j].second;
+                if (c == INT_MAX) {
+                    cagou = true;
+                    break;
+                } else {
+                    o += c;
+                }
             }
-            if (i != n) hi = mid;
-            else lo = mid;*/
-
+            if (!cagou) {
+                if (o <= b)
+                    lo = mid;
+                else
+                    hi = mid - 1;
+            } else hi = mid - 1;
         }
-        cout << mid << '\n';
+        cout << lo << '\n';
     }
 }
