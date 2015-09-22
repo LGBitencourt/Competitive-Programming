06f9cb3393a471c7c0f1707611b14e7887f7e18c
Luis Gustavo Bitencourt Almeida <ahluis@europa.eclipse.ime.usp.br>
2015-09-22 17:18:38 -0300

    321 b div 2 final

--- codeforces/580b.cpp
+++ codeforces/580b.cpp
@@ -11,7 +11,8 @@ typedef pair<int, int> ppi;
 
 int main () {
     ios::sync_with_stdio(false);
-    int n, d, sum = 0, max = 0;
+    int n, d;
+    long long int sum = 0, max = 0;
     ppi p[100005];
     cin >> n >> d;
     for (int i = 0; i < n; i++) {
@@ -23,7 +24,7 @@ int main () {
     int i = 0;
     for (int j = 0; j < n; j++) {
         sum += p[j].ss;
-        while (p[j].ff - p[i].ff > d) sum -= p[i].ss, i++;
+        while (p[j].ff - p[i].ff >= d) sum -= p[i].ss, i++;
         if (sum > max) max = sum;
     }
     cout << max << '\n';
