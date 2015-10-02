791ed15407963f0d870e324c2bc4ed1157cfcbf4
Luis Gustavo Bitencourt Almeida <ahluis@hubble.eclipse.ime.usp.br>
2015-10-02 13:00:04 -0300

    dfs spoj

--- spoj/natureza.cpp
+++ spoj/natureza.cpp
@@ -6,25 +6,34 @@
 
 using namespace std;
 
-int c, r, mx;
+int c, r, mx, path;
 map<string, int> index;
 string name, x, y;
 vector<int> adj[5005];
 
 bool visited[5005];
 
-void dfs(int v, int m) {
+void dfs(int v) {
+    path++;
     visited[v] = true;
-    if (m > mx) mx = m;
-    for (vector<int>::iterator it = adj[v].begin(); it != adj[v].end(); ++it)
-        if (!visited[*it])
-            dfs(*it, m + 1);
+    for (int i = 0; i < adj[v].size(); i++)
+        if (!visited[adj[v][i]])
+            dfs(adj[v][i]);
+}
+
+void zera() {
+    for (int i = 0; i < c; i++)
+        visited[i] = false;
+    index.clear();
+    for (int i = 0; i < c; i++)
+        adj[i].clear();
 }
 
 int main () {
     ios::sync_with_stdio(false);
     while (cin >> c >> r, c != 0) {
         mx = 0;
+        zera();
         for (int i = 0; i < c; i++) {
             cin >> name;
             index.insert(make_pair(name, i));
@@ -32,9 +41,15 @@ int main () {
         for (int i = 0; i < r; i++) {
             cin >> x >> y;
             adj[index[x]].push_back(index[y]);
+            adj[index[y]].push_back(index[x]);
         }
-        for (int i = 0; i < c; i++)
-            dfs(i, 1);
+        for (int i = 0; i < c; i++) {
+            if (!visited[i]) {
+                path = 0;
+                dfs(i);
+                if (path > mx) mx = path;
+            }
+        }
         cout << mx << '\n';
     }
 }
