a6a5ec65a71ea97a3472226fdc56d135746bd2b1
Gustavo Bitencourt <lg.bitencourt@gmail.com>
2015-10-17 20:07:23 -0300

    regional sulamerica

--- acm-icpc/triangles.cpp
+++ acm-icpc/triangles.cpp
@@ -4,26 +4,43 @@
 
 using namespace std;
 
+struct P {
+    long long int x, y, norm;
+};
+
+P anc;
+
 bool compare (P a, P b) {
-    if (abs(a) != abs(b)) return abs(a) < abs(b);
-    else return a.x < b.x;
+    if (a.norm != b.norm) return a.norm < b.norm;
+    return a.x < b.x;
 }
 
 int main () {
-    int n;
+    int n, t;
     while (scanf (" %d", &n), n != 0) {
-        int t = 0;
-        complex<int> p[1005];
+        t = 0;
+        P ps[1005], p[1005];
         for (int i = 0; i < n; i++) {
-            int x, y;
-            scanf (" %d %d", &x, &y);
+            long long int x, y;
+            scanf (" %lld %lld", &x, &y);
+            p[i].x = ps[i].x = x;
+            p[i].y = ps[i].y = y;
         }
         for (int i = 0; i < n; i++) {
-            p[1004] = p[i];
-            sort(p, p + n, compare);
-            for (int i = 1; i < n; i++)
-                if (p[i].norm == p[i-1].norm)
-                    t += n * (n - 1) / 2;
+            anc = p[i];
+            for (int j = 0; j < n; j++)
+                ps[j].norm = (anc.x - ps[j].x)*(anc.x - ps[j].x) + (anc.y - ps[j].y)*(anc.y - ps[j].y);
+            sort (ps, ps + n, compare);
+            int k = 1;
+            for (int j = 1; j < n; j++) {
+                if (ps[j].norm == ps[j-1].norm) {
+                    k++;
+                } else {
+                    t += k*(k-1)/2;
+                    k = 1;
+                }
+            }
+            t += k*(k-1)/2;
         }
         printf ("%d\n", t);
     }
