f1d49a05f62924fdeb76c3a6c5aaf8a3a63c4f20
Luis Gustavo Bitencourt Almeida <ahluis@hubble.eclipse.ime.usp.br>
2015-10-19 15:34:09 -0200

    wa no powerplants

---
+++ acm-icpc/powerplant.cpp
@@ -0,0 +1,55 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+struct P {
+    int u, v, c;
+} p[4000005];
+
+int n, m, k, sum;
+
+int pai[2005], pp[2005];
+
+bool compare (P a, P b) { return a.c < b.c; }
+
+int find (int v) {
+    if (pai[v] == -1) return v;
+    return pai[v] = find(pai[v]);
+}
+
+bool join (int a, int b) {
+    if (!binary_search(pp, pp + k, find(a)) && find(a) != find(b)) {
+        pai[find(a)] = find(b);
+        return true;
+    } else if (!binary_search(pp, pp + k, find(b)) && find(a) != find(b)) {
+        pai[find(b)] = find(a);
+        return true;
+    }
+    return false;
+}
+
+int main () {
+    int t;
+    scanf (" %d", &t);
+    for (int j = 1; j <= t; j++) {
+        sum = 0;
+        scanf (" %d %d %d", &n, &m, &k);
+        for (int i = 0; i <= n; i++)
+            pai[i] = -1;
+
+        for (int i = 0; i < k; i++)
+            scanf (" %d", &pp[i]);
+
+        for (int i = 0; i < m; i++)
+            scanf (" %d %d %d", &p[i].u, &p[i].v, &p[i].c);
+
+        sort(p, p + m, compare);
+
+        for (int i = 0; i < m; i++)
+            if (join(p[i].u, p[i].v))
+                sum += p[i].c;
+
+        printf ("Case #%d: %d\n", j, sum);
+    }
+}
