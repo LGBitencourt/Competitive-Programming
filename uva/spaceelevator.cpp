b285af10b8e53e7482c1ba5d9b9714d03ba8eba9
Luis Gustavo Bitencourt Almeida <lgbitencourt@mario.linux.ime.usp.br>
2015-11-17 12:06:13 -0200

    pd nos digitos

--- uva/spaceelevator.cpp
+++ uva/spaceelevator.cpp
@@ -1,36 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+typedef unsigned long long ll;
 
 char str[20];
 ll mdp[20][15][2][2];
 
-ll dp (int i, int last, bool x, bool pre) {
+ll dp (int i, int last, int pre, int x) {
     if (str[i] == 0) return x;
-    ll &m = mdp[i][last][x][pre];
+    ll &m = mdp[i][last][pre][x];
     if (m != -1) return m;
     m = 0;
-    bool f = x;
     for (int d = 0; d <= 9; d++) {
         if (pre && d > str[i] - '0') break;
-        bool c = x;
+        int c = x;
         if (d == 4 || (last == 1 && d == 3)) c = 1;
-        m += dp (i + 1, d, c, pre && (str[i] - '0' == d));
+        m += dp (i + 1, d, pre && str[i] - '0' == d, c);
     }
     return m;
 }
 
 int main () {
     ll n;
-    while (scanf (" %lld", &n) != EOF) {
-        ll l = 0, h = 1e+19;
+    while (scanf (" %llu", &n) != EOF) {
+        ll l = 0, h = 10000000000000000000;
         while (l != h) {
             ll mid = l + (h-l)/2;
             memset(mdp, -1, sizeof mdp);
-            sprintf (str, "%lld", mid);
-            if (mid - dp(0, 0, false, true) >= n) h = mid;
+            sprintf (str, "%llu", mid);
+            if (mid - dp(0, 0, 1, 0) >= n) h = mid;
             else l = mid + 1;
         }
-        printf ("%lld\n", l);
+        printf ("%llu\n", l);
     }
 }
+
