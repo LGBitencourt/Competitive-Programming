5731ddca548dca3562105725437643d6138b9483
Gustavo Bitencourt <lg.bitencourt@gmail.com>
2016-01-21 18:10:01 -0200

    finalmente aprendi seg

--- acm-icpc/intervalproduct.cpp
+++ acm-icpc/intervalproduct.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int seg[500005];
+int N, K, X, Y;
+char c;
 
 void update (int l, int r, int i, int v, int n) {
     if (i < l || i > r) return;
@@ -11,10 +13,44 @@ void update (int l, int r, int i, int v, int n) {
     }
     update (l, (l+r)/2, i, v, 2*n);
     update (((l+r)/2)+1, r, i, v, (2*n)+1);
-    seg[n] = min (seg[2*n], seg[(2*n)+1]);
+    seg[n] = seg[2*n]*seg[(2*n) + 1];
 }
 
-int query (int x, int y, int l, int r, int n) {
-    if (x >= l && y <= r) return seg[n];
-    if (x > r || y < l) return
+int query (int l, int r, int x, int y, int n) {
+    if (l >= x && r <= y) return seg[n];
+    if (r < x || l > y) return 1;
+    return query (l, (l+r)/2, x, y, 2*n)
+        * query (((l+r)/2) + 1, r, x, y, (2*n) + 1);
+}
+
+void update(int i, int v) {
+    if (v < 0) v = -1;
+    else if (v > 0) v = 1;
+    else v = 0;
+    update(0, N-1, i, v, 1);
+}
+int query (int l, int r) {
+    return query (0, N-1, l, r, 1);
+}
+
+int main () {
+    while (scanf (" %d %d", &N, &K) != EOF) {
+        memset (seg, 1, sizeof seg);
+        for (int i = 0; i < N; i++) {
+            int a;
+            scanf (" %d", &a);
+            update (i, a);
+        }
+        for (int i = 0; i < K; i++) {
+            scanf (" %c %d %d", &c, &X, &Y);
+            if (c == 'C') update (X-1, Y);
+            else if (c == 'P') {
+                int a = query (X-1, Y-1);
+                if (a > 0) putchar ('+');
+                else if (a < 0) putchar ('-');
+                else putchar ('0');
+            }
+        }
+        putchar ('\n');
+    }
 }
--- spoj/rpln.cpp
+++ spoj/rpln.cpp
@@ -1,54 +1,42 @@
-#include <cstdio>
-#include <algorithm>
-#include <climits>
-
-#define MAX 1000000
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int tree[MAX];
-int input[MAX];
+int seg[500005];
 
-void update(int node, int start, int end, int k, int val) {
-    if (start == end) {
-        tree[node] = val;
-    } else {
-        int mid = (start+end)/2;
-        if(k <= mid)
-            update(2*node, start, mid, k, val);
-        else
-            update(2*node + 1, mid + 1, end, k, val);
-        tree[node] = min(tree[2*node], tree[2*node + 1]);
+void update (int l, int r, int i, int v, int n) {
+    if (i < l || i > r) return;
+    if (l == r) {
+        seg[n] = v;
+        return;
     }
+    update (l, (l+r)/2, i, v, 2*n);
+    update (((l+r)/2)+1, r, i, v, (2*n)+1);
+    seg[n] = min (seg[2*n], seg[(2*n)+1]);
 }
 
-int query(int node, int start, int end, int l, int r) {
-    if (r < start || l > end) {
-        return INT_MAX;
-    } else if (l <= start && r >= end) {
-        return tree[node];
-    } else {
-        int mid = start + (end - start)/2;
-        return min(query(node*2, start, mid, l, r), 
-            query(node*2 + 1, mid + 1, end, l, r));
-    }
+int query (int x, int y, int l, int r, int n) {
+    if (x >= l && y <= r) return seg[n];
+    if (x > r || y < l) return INT_MAX;
+    return min (query (x, (x+y)/2, l, r, 2*n),
+            query (((x+y)/2)+1, y, l, r, (2*n)+1));
 }
 
-int main() {
+int main () {
     int t, n, q;
-    scanf("%d", &t);
-    for (int j = 1; j <= t; j++) {
-        printf("Scenario #%d:\n\n", j);
-        scanf("%d %d", &n, &q);
-        for (int i = 1; i <= n; i++) {
-            int x;
-            scanf(" %d", &x);
-            update(1, 1, n, i, x);
+    scanf (" %d", &t);
+    for (int tc = 1; tc <= t; tc++) {
+        scanf (" %d %d", &n, &q);
+        for (int i = 0; i < n; i++) {
+            int v;
+            scanf (" %d", &v);
+            update (0, n-1, i, v, 1);
         }
+        printf ("Scenario #%d:\n", tc);
         for (int i = 0; i < q; i++) {
             int l, r;
-            scanf("%d %d", &l, &r);
-            printf("%d\n\n", query(1, 1, n, l, r));
+            scanf (" %d %d", &l, &r);
+            printf ("%d\n", query (0, n-1, l-1, r-1, 1));
         }
     }
 }
+
