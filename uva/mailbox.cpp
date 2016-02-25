9cc19f7a59d9163ac867e0573e01881490007919
Gustavo Bitencourt <lg.bitencourt@gmail.com>
2016-02-24 23:26:29 -0300

    como o thiago entendeu isso no contest?

--- uva/mailbox.cpp
+++ uva/mailbox.cpp
@@ -7,8 +7,18 @@ int dp (int k, int i, int j) {
     if (i == j) return i;
     if (k == 1) return (j*(j+1)/2) - (i*(i-1)/2);
     if (mdp[k][i][j] != -1) return mdp[k][i][j];
-    int ans = INT_MIN;
-    for (int l = i; l <= j; l++) {
-        int aux = l + max (dp (k-1,
+    int ans = INT_MAX;
+    for (int l = i; l <= j; l++)
+        ans = min (ans, max (l+dp (k-1, i, l-1), l+dp (k, l+1, j)));
+    return mdp[k][i][j] = ans;
+}
+
+int main () {
+    int t, k, m;
+    scanf (" %d", &t);
+    while (t--) {
+        memset (mdp, -1, sizeof mdp);
+        scanf (" %d %d", &k, &m);
+        printf ("%d\n", dp (k, 1, m));
     }
 }
