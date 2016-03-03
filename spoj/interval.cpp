344035f3798d7cd55cbf7740ac8e9062b63898e5
Gustavo Bitencourt <lg.bitencourt@gmail.com>
2016-03-02 21:52:58 -0300

    interval mito

--- spoj/interval.cpp
+++ spoj/interval.cpp
@@ -2,45 +2,41 @@
 #define MAX 50005
 using namespace std;
 
-int bit[MAX], uf[MAX], c[MAX];
-struct germano {
-    int a, b, c;
-} p[MAX];
+int bit[MAX], uf[MAX], a[MAX], b[MAX], c[MAX], p[MAX];
 
-bool cmp (germano a, germano b) {
-    if (a.b != b.b) return a.b < b.b;
-    return a.a < b.a;
-}
+bool cmp (int i, int j) { return b[i] < b[j]; }
 
 void update (int idx) {
-    for (int i = idx; i < MAX; i += i & -i)
+    for (int i = idx+1; i < MAX; i += i&-i)
         bit[i] += 1;
 }
 
 int query (int idx) {
     int sum = 0;
-    for (int i = idx; i > 0; i -= i & -i)
+    for (int i = idx+1; i > 0; i -= i&-i)
         sum += bit[i];
     return sum;
 }
 
 int main () {
-    int t, n, a, b, ci, ans;
+    int t, n, ans;
     scanf (" %d", &t);
     while (t--) {
         ans = 0;
         scanf (" %d", &n);
-        for (int i = 1; i < MAX; i++)
+        for (int i = 0; i < MAX; i++)
             uf[i] = i-1, bit[i] = 0;
         for (int i = 0; i < n; i++) {
-            scanf (" %d %d %d", &a, &b, &ci);
-            a++, b++;
-            p[i].a = a, p[i].b = b, p[i].c = ci;
+            scanf (" %d %d %d", &a[i], &b[i], &c[i]);
+            p[i] = i;
         }
         sort (p, p + n, cmp);
         for (int i = 0; i < n; i++) {
-            while (query (p[i].b) - query (p[i].a-1) < p[i].c) {
-                update (uf[p[i].b+1]), uf[p[i].b+1] = uf[uf[p[i].b+1]], p[i].c--, ans++;
+            int q = c[p[i]] - query(b[p[i]]) + query(a[p[i]]-1);
+            while (q > 0){
+                update(uf[b[p[i]]+1]);
+                uf[b[p[i]]+1] = uf[uf[b[p[i]]+1]];
+                q--, ans++;
             }
         }
         printf ("%d\n", ans);
