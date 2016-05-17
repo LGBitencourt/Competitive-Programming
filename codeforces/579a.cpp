2112fa7406ccc202161d5b110ef50835c14a4dc5
Gustavo Bitencourt <lg.bitencourt@gmail.com>
2016-05-16 23:25:01 -0300

    esse foi o maior roubo com c++ q eu ja fiz

---
+++ codeforces/579a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define mp make_pair
+#define pb push_back
+#define ff first
+#define ss second
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+const double eps = 1e-9;
+const int inf = INT_MAX;
+////////////////0123456789
+const int MAX = 10004;
+const int modn = 1000000007;
+
+int main() {
+    int n;
+    scanf (" %d", &n);
+    printf("%d\n", __builtin_popcount(n));
+}
+
