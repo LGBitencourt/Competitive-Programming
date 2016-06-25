5e68c5e65f7591d945e49b2c64af9606b57c82a2
Gustavo Bitencourt <lg.bitencourt@gmail.com>
2016-06-25 16:36:36 -0300

    que beleza sogrinha

--- uri/1474.cpp
+++ uri/1474.cpp
@@ -19,18 +19,33 @@ const int modn = 1000000007;
 ll N, K, L;
 
 matrix mul(matrix A, matrix B) {
-    matrix C(K+1, vector<ll>(K+1));
-    for (int i = 0; i < 2; i++)
-        for (int j = 0; j < 2; j++)
-            for (int k = 0; k < 2; k++)
+    matrix C(3, vector<ll>(3));
+    for (int i = 1; i < 3; i++)
+        for (int j = 1; j < 3; j++)
+            for (int k = 1; k < 3; k++)
                 C[i][j] = (C[i][j] + A[i][k] * B[k][j]) % 1000000;
     return C;
 }
 
-
+matrix pow(matrix A, ll p) {
+    if (p == 1) return A;
+    if (p % 2) return mul(A, pow(A, p-2));
+    matrix X = pow(A, p/2);
+    return mul(X, X);
+}
 
 int main() {
     scanf (" %lld %lld %lld", &N, &K, &L);
-    printf("%.06lld\n", f(N/5));
+    vector<ll> f(3);
+    f[1] = K;
+    f[2] = K*K + L;
+    matrix T(3, vector<ll>(3));
+    T[1][1] = 0, T[1][2] = 1;
+    T[2][1] = K, T[2][2] = L;
+    T = pow(T, N/5);
+    ll ans = 0;
+    for (int i = 1; i < 3; i++)
+        ans = (ans + T[1][i] * f[i]) % 1000000;
+    printf("%.06lld\n", ans);
 }
 
