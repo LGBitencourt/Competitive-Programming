23234dae5719a97280d8f08d9cdb7fdc0bb5b62b
Gustavo Bitencourt <lg.bitencourt@gmail.com>
2016-08-01 22:30:56 -0300

    ta cagado

--- codeforces/101041j.cpp
+++ codeforces/101041j.cpp
@@ -15,18 +15,44 @@ const int inf = INT_MAX;
 const int MAX = 20;
 const int modn = 1000000007;
 
+int n, M;
+pii coins[MAXN];
+char grid[55][55];
 int dist[MAXN][MAXN];
-
+int mdp[MAXN][MAXN];
 
 int dp (int v, int mask) {
-    if (__builtin_popcount(mask) == M && v == D) return 0;
+    if (__builtin_popcount(mask) == M) return dist[v][0];
     if (mdp[v][mask] != -1) return mdp[v][mask];
-    int ans = 0;
-    for (int i = 0; i < N; i++)
+    int ans = inf;
+    for (int i = 0; i < M; i++)
         if (((1 << i) & mask) == 0)
+            ans = min (ans, dist[v][i] + dp (i, masrk | (1 << i)));
+    return mdp[v][mask] = ans;
+}
 
+void fill_dist() {
+    priority_queue<pii> q;
+    for (int i = 0; i < M; i++) {
+        dist[i][i] = 0;
+        q.push(0, i);
+        while (!q.empty()) {
+            pii c = q.top();
+            q.pop();
+
+        }
+    }
 }
 
 int main() {
+    scanf (" %d", &n);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++) {
+            scanf (" %c", &grid[i][j]);
+            if (grid[i][j] == 'M') coins[M++] = pii(i, j);
+        }
+    fill_dist();
+    memset (mdp, -1, mdp);
+    printf("%d\n", dp(0, 0));
 }
 
